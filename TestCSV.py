027894f0e66f3da971eb4a3f6317ae4bf1d00842
mkritzl-tgm <mkritzl@student.tgm.ac.at>
2016-02-13 11:35:16 +0100

    Willfully changed test to bad behavior

--- TestCSV.py
+++ TestCSV.py
@@ -25,7 +25,7 @@ class TestCSV(unittest.TestCase):
         self.csvHandler.appendLine(["Neue Zeile", "2"])
         self.assertEqual("Neue Zeile;1", self.csvHandler.getLineAsString(1547))
         # self.assertEqual("Neue Zeile;2", self.csvHandler.getLineAsString(1548))
-        self.assertEqual("Neue Zeile;2", self.csvHandler.getLineAsString(1548))
+        self.assertEqual("Neue Zeile;3", self.csvHandler.getLineAsString(1548))
 
     def testSetContent(self):
         self.csvHandler.setContent("Das ist alles")
