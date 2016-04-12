e72fa54d3957a28d7f68f1276362aa8fda40afea
mkritzl-tgm <mkritzl@student.tgm.ac.at>
2016-04-12 14:23:04 +0200

    Bufix in csv-parser (last column was cut)

--- csvhandler/CSVHandler.py
+++ csvhandler/CSVHandler.py
@@ -14,45 +14,45 @@ class CSVHandler(object):
                 rows.append(row[0:len(row)-1])
             return rows
 
-    def getContentAsString(self, file):
-        output = ""
-        rows = self.getContentAsArray(file)
-        for row in rows:
-            output = output + ';'.join(row[0:len(row)-1]) + '\r\n'
+    # def getContentAsString(self, file):
+    #     output = ""
+    #     rows = self.getContentAsArray(file)
+    #     for row in rows:
+    #         output = output + ';'.join(row[0:len(row)-1]) + '\r\n'
+    #
+    #     return output
 
-        return output
+    # def getLineAsArray(self, file, linenumber):
+    #     linenumber = linenumber - 1
+    #     with open(file, newline='') as csvfile:
+    #         file = csv.reader(csvfile, delimiter=';')
+    #         if linenumber is None:
+    #             collections.deque(file, maxlen=0)
+    #         else:
+    #             next(islice(file, linenumber, linenumber), None)
+    #
+    #         return next(file)
 
-    def getLineAsArray(self, file, linenumber):
-        linenumber = linenumber - 1
-        with open(file, newline='') as csvfile:
-            file = csv.reader(csvfile, delimiter=';')
-            if linenumber is None:
-                collections.deque(file, maxlen=0)
-            else:
-                next(islice(file, linenumber, linenumber), None)
+    # def getLineAsString(self, file, linenumber):
+    #     linenumber = linenumber - 1
+    #     with open(file, newline='') as csvfile:
+    #         file = csv.reader(csvfile, delimiter=';')
+    #         if linenumber is None:
+    #             collections.deque(file, maxlen=0)
+    #         else:
+    #             next(islice(file, linenumber, linenumber), None)
+    #
+    #
+    #         tmp = next(file)
+    #         return ';'.join(tmp)
 
-            return next(file)
-
-    def getLineAsString(self, file, linenumber):
-        linenumber = linenumber - 1
-        with open(file, newline='') as csvfile:
-            file = csv.reader(csvfile, delimiter=';')
-            if linenumber is None:
-                collections.deque(file, maxlen=0)
-            else:
-                next(islice(file, linenumber, linenumber), None)
-
-
-            tmp = next(file)
-            return ';'.join(tmp)
-
-    def clearFile(self, fromFile, toFile):
-        with open(fromFile, newline='') as csvSource, open(toFile, "w", newline='') as csvDest:
-            reader = csv.reader(csvSource, delimiter=';')
-            writer = csv.writer(csvDest, delimiter=';')
-
-            for row in reader:
-                writer.writerow(row[0:len(row)-1])
+    # def clearFile(self, fromFile, toFile):
+    #     with open(fromFile, newline='') as csvSource, open(toFile, "w", newline='') as csvDest:
+    #         reader = csv.reader(csvSource, delimiter=';')
+    #         writer = csv.writer(csvDest, delimiter=';')
+    #
+    #         for row in reader:
+    #             writer.writerow(row[0:len(row)-1])
 
     def setContent(self, file, content):
         with open(file, "w", newline='') as csvfile:
@@ -61,15 +61,16 @@ class CSVHandler(object):
 
             if isinstance(content, list):
                 for row in content:
+                    row.append("")
                     writer.writerow(row)
-            elif type(content) is str:
-                writer.writerow(content.split(";"))
+            # elif type(content) is str:
+            #     writer.writerow(content.split(";"))
 
-    def appendLine(self, file, row):
-        with open(file, "a", newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=';')
-
-            if isinstance(row, list):
-                writer.writerow(row)
-            elif type(row) is str:
-                writer.writerow(row.split(";"))
+    # def appendLine(self, file, row):
+    #     with open(file, "a", newline='') as csvfile:
+    #         writer = csv.writer(csvfile, delimiter=';')
+    #
+    #         if isinstance(row, list):
+    #             writer.writerow(row)
+    #         elif type(row) is str:
+    #             writer.writerow(row.split(";"))
